b510ef79a75077cc6859ea0e21cf8611b382f3d3
ureishi <57707826+ureishi@users.noreply.github.com>
2020-06-12 00:57:29 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 require 'twitter'
 
-ENV = {
+ENVIRONMENT = {
 'TWITTER_CONSUMER_KEY' => ARGV[0],
 'TWITTER_CONSUMER_SECRET' => ARGV[1],
 'TWITTER_ACCESS_TOKEN' => ARGV[2],
@@ -9,10 +9,10 @@ ENV = {
 $client
 def twitter_client
   return $client ||= Twitter::REST::Client.new{|config|
-    config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-    config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    config.consumer_key = ENVIRONMENT['TWITTER_CONSUMER_KEY']
+    config.consumer_secret = ENVIRONMENT['TWITTER_CONSUMER_SECRET']
+    config.access_token = ENVIRONMENT['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENVIRONMENT['TWITTER_ACCESS_TOKEN_SECRET']
   }
 end
 
