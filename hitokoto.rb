5b07d29c0ee896a9355526ad958857aa5465a980
ureishi <57707826+ureishi@users.noreply.github.com>
2020-06-12 10:01:26 +0900

    Update hitokoto.rb

--- hitokoto.rb
+++ hitokoto.rb
@@ -40,7 +40,8 @@ result_tweets.take(100).each{|tw|
 	puts "screen_name: #{tw.user.screen_name}"
 	puts "name: #{tw.user.name}"
 	puts "id: #{tw.user.id}"
-	puts "media:\n#{tw.media.map{"#{_1}?format=jpg&name=orig"}.join "\n"}"
+	puts "full_text: #{tw.full_text}"
+	puts "media:\n#{tw.media.map{"#{_1.media_uri_https}?format=jpg&name=orig"}.join "\n"}"
 	puts
 }
 
