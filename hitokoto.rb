1f0dd935be9051363d47e72784cd31d50e28c551
ureishi <57707826+ureishi@users.noreply.github.com>
2020-06-13 12:20:02 +0900

    Update hitokoto.rb

--- hitokoto.rb
+++ hitokoto.rb
@@ -41,7 +41,7 @@ puts
 
 image_uri = nil
 result_tweets.take(N).each{|tw|
-	if true || whitelist.include? tw.user.id
+	if true or whitelist.include? tw.user.id
 		sleep 10
 		t = (if tw.retweet?
 			tw.retweeted_status
