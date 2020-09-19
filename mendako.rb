ce94b5b133821d5a618dd0f2548a1a55a9ffe0b4
ureishi <victorique2020+github@gmail.com>
2020-09-20 01:17:18 +0900

    update

--- mendako.rb
+++ mendako.rb
@@ -1,4 +1,5 @@
 require 'twitter'
+require 'mini_magick'
 
 ENVIRONMENT = {
 	TWITTER_CONSUMER_KEY: ARGV[0],
@@ -29,7 +30,7 @@ allowlist = File.open('allowlist.txt'){|f|
 }
 puts "allowlist: #{allowlist}"
 
-N = 2
+N = 10
 
 result_tweets = twitter_client.search(
 	query,
@@ -41,49 +42,103 @@ puts
 
 auto_mode = true
 
-image_uri = nil
-if auto_mode
-	result_tweets.take(N).each{|tw|
-
-		if allowlist.include? tw.user.id #or true
-			sleep 10
-			t = (if tw.retweet?
-				tw.retweeted_status
-			else
-				twitter_client.status(tw, tweet_mode: 'extended')
-			end)
-
-			if t.media?
-				image_uri = get_orig_image_uri t.media.first.media_uri_https
-			elsif t.uris.length > 0
-				t.uris.each{
-					if _1.expanded_url.to_s.start_with? 'https://twitter.com'
-						sleep 5
-						t = twitter_client.status(_1.expanded_url, tweet_mode: 'extended')
-						if t.media?
-							image_uri = get_orig_image_uri t.media.first.media_uri_https
-						end
-					end
-					break if image_uri
-				}
-			end
+image_uri = []
+result_tweets.take(N).each{|tw|
+	if allowlist.include? tw.user.id #or true
+		sleep 10
+		t = if tw.retweet?
+			tw.retweeted_status
+		else
+			twitter_client.status tw, tweet_mode: 'extended'
 		end
 
-		break if image_uri
-	}
-else
-	sleep 5
-	t = twitter_client.status(File.open('manual_url.txt'){_1.gets}.chomp, tweet_mode: 'extended')
-	if t.media?
-		image_uri = get_orig_image_uri t.media.first.media_uri_https
+		if t.media?
+			t.media.each{
+				image_uri << get_orig_image_uri _1.media_uri_https
+			}
+		end
 	end
-end
 
-image_uri ||= 'https://ureishi.github.io/mendako/output.jpg'
+	break if image_uri.length >= 4
+}
 
-puts "image_uri: #{image_uri}"
+is_new = [true, false, false, false]
 
-file_name = 'output.jpg'
-# 試行回数3回 タイムアップ3秒 待機時間3秒
-command = "wget -t 3 -T 3 -w 3 -O #{file_name} #{image_uri}"
-`#{command}`
+puts "image_uri:\n\t#{image_uri.join "\n"}"
+
+### create image
+4.times{|page|
+	image_url = image_uri[page]
+
+	BASE_W = 1024
+	BASE_H = 1448
+	SIZE = 1024
+
+	image = MiniMagick::Image.open 'base.png'
+	image.resize "#{BASE_W}x#{BASE_H}!"
+	image.format 'png'
+
+	image.combine_options{
+		pos_nw = '0, 0'
+		pos_se = "#{BASE_W}, #{BASE_H}"
+		_1.fill '#fcece1'
+		_1.draw "rectangle #{pos_nw} #{pos_se}"
+	}
+
+	image_base = image
+
+	image_over = MiniMagick::Image.open image_url
+	#image_over = MiniMagick::Image.open 'mendako_none.png'
+	image_over.resize "#{SIZE}x#{SIZE}"
+
+	image = image_base.composite image_over{
+		_1.compose 'Over'
+		_1.gravity 'Center'
+		_1.geometry "+0+0"
+	}
+
+	image.combine_options{
+		pos = '0, 30'
+		text = 'まいにちめんだこ'
+		_1.font './.font/memoir-round.otf'
+		_1.fill '#f9832c'
+		_1.gravity 'North'
+		_1.pointsize 125
+		_1.stroke '#ffffff'
+		_1.strokewidth 3
+		_1.draw "text #{pos} '#{text}'"
+	}
+
+	image.combine_options{
+		pos = '55, 160'
+		text = '@daily_mendako'
+		_1.font '.font/Noto_Sans_JP/NotoSansJP-Regular.otf'
+		_1.fill '#000000'
+		_1.gravity 'NorthEast'
+		_1.pointsize 30
+		_1.draw "text #{pos} '#{text}'"
+	}
+
+	NEW_ICON_X = [-146, -48, 50, 148]
+	4.times{|i|
+		next if !is_new[i]
+		image.combine_options{
+			pos = "#{NEW_ICON_X[i]}, 625"
+			text = 'NEW!'
+			_1.font './.font/memoir-round.otf'
+			_1.fill '#a31d12'
+			_1.gravity 'Center'
+			_1.pointsize 25
+			_1.draw "text #{pos} '#{text}'"
+		}
+	}
+
+	image.combine_options{
+		pos_nw = "0, #{BASE_H - 88}"
+		pos_se = "#{BASE_W}, #{BASE_H}"
+		_1.fill '#ffc575'
+		_1.draw "rectangle #{pos_nw} #{pos_se}"
+	}
+
+	image.write "public/mendako#{page}.png"
+}
