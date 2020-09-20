cc2854509ded5e4ce2b174b243a52a892528f8ad
ureishi <victorique2020+github@gmail.com>
2020-09-21 01:49:07 +0900

    Update mendako.rb

--- mendako.rb
+++ mendako.rb
@@ -74,57 +74,61 @@ MiniMagick::Tool::Convert.new{|c|
 	c << 'base.png'
 }
 
+base_image = MiniMagick::Image
+.open('base.png')
+.combine_options{
+	pos = '0, 30'
+	text = 'まいにちめんだこ'
+	_1.font './.font/memoir-round.otf'
+	_1.fill '#f9832c'
+	_1.gravity 'North'
+	_1.pointsize 125
+	_1.stroke '#ffffff'
+	_1.strokewidth 3
+	_1.draw "text #{pos} '#{text}'"
+}
+.combine_options{
+	pos = '55, 160'
+	text = '@daily_mendako'
+	_1.font '.font/Noto_Sans_JP/NotoSansJP-Regular.otf'
+	_1.fill '#000000'
+	_1.gravity 'NorthEast'
+	_1.pointsize 30
+	_1.draw "text #{pos} '#{text}'"
+}
+.combine_options{
+	pos_nw = "0, #{BASE_H - 88*2}"
+	pos_se = "#{BASE_W}, #{BASE_H}"
+	_1.fill '#ffc575'
+	_1.draw "rectangle #{pos_nw} #{pos_se}"
+}
+
+$count_new.times{|i|
+	image.combine_options{
+		pos = "#{NEW_ICON_X[i]*2}, 536"
+		text = 'NEW!'
+		_1.font './.font/memoir-round.otf'
+		_1.fill '#a31d12'
+		_1.gravity 'Center'
+		_1.pointsize 25
+		_1.draw "text #{pos} '#{text}'"
+	}
+}
+
+image.write 'base.png'
+puts "created: base.png"
+
 4.times{|p|
-	image_url = $image_uri[p]
-	
 	image = MiniMagick::Image
 	.open('base.png')
 	.composite(
 		MiniMagick::Image
-		.open(image_url || 'not_found.png')
+		.open($image_uri[p] || 'not_found.png')
 		.resize("#{SIZE}x#{SIZE}")
 	){
 		_1.compose 'Over'
 		_1.gravity 'Center'
 		_1.geometry "+0+0"
-	}
-	.combine_options{
-		pos = '0, 30'
-		text = 'まいにちめんだこ'
-		_1.font './.font/memoir-round.otf'
-		_1.fill '#f9832c'
-		_1.gravity 'North'
-		_1.pointsize 125
-		_1.stroke '#ffffff'
-		_1.strokewidth 3
-		_1.draw "text #{pos} '#{text}'"
-	}
-	.combine_options{
-		pos = '55, 160'
-		text = '@daily_mendako'
-		_1.font '.font/Noto_Sans_JP/NotoSansJP-Regular.otf'
-		_1.fill '#000000'
-		_1.gravity 'NorthEast'
-		_1.pointsize 30
-		_1.draw "text #{pos} '#{text}'"
-	}
-	.combine_options{
-		pos_nw = "0, #{BASE_H - 88*2}"
-		pos_se = "#{BASE_W}, #{BASE_H}"
-		_1.fill '#ffc575'
-		_1.draw "rectangle #{pos_nw} #{pos_se}"
-	}
-	
-	$count_new.times{|i|
-		image.combine_options{
-			pos = "#{NEW_ICON_X[i]*2}, 536"
-			text = 'NEW!'
-			_1.font './.font/memoir-round.otf'
-			_1.fill '#a31d12'
-			_1.gravity 'Center'
-			_1.pointsize 25
-			_1.draw "text #{pos} '#{text}'"
-		}
 	}
 	
 	image.write "public/mendako#{p}.png"
