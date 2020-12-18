a7dc151ad5ee7dc7ce4d279e95161e1b112b14fd
ureishi <victorique2020+github@gmail.com>
2020-12-18 16:24:12 +0900

    w

--- mendako.rb
+++ mendako.rb
@@ -126,7 +126,7 @@ image_uri.length.times{|p|
 	}
 	.combine_options{
 		pos = '0, 160'
-		text = "@daily_mendako\t(#{created_at[p].strftime("%Y年%m月%d日 %H時%M分%S秒 JST")})"
+		text = "@daily_mendako\t(#{created_at[p].getlocal('+09:00').strftime("%Y年%m月%d日 %H時%M分%S秒 JST")})"
 		_1.font '.font/Noto_Sans_JP/NotoSansJP-Regular.otf'
 		_1.fill '#000000'
 		_1.gravity 'North'
